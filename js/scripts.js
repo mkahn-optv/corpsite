42200518a0894c4d0206d1271b5fec27e6582b5b
jadams41 <theride40@gmail.com>
2016-01-20 15:50:35 -0800

    Changed the picture layout in the engage section

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,17 @@ $(document).ready(function () {/* smooth scrolling for scroll to top */
     /* highlight the top nav as scrolling occurs */
     $('body').scrollspy({target: '#navbar'})
 
+    /*var i=0;
+    var root = document.querySelector('.fadein');
+    var els = root.querySelectorAll(':not(:first-child)');
+    for (i=0; i < els.length; i++) {
+        els[i].classList.add('is-hidden');
+    }
+    root.addEventListener('transitionend', function(){
+        root.insertBefore(root.querySelector(':first-child.is-hidden'), null);
+    });
+    setInterval(function(){
+        root.querySelector(':first-child').classList.add('is-hidden');
+        root.querySelector(':nth-child(2)').classList.remove('is-hidden');
+    }, 3000)*/
 });
