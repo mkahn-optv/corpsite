2de035b1f8f8cacf8e175429fd062d0978d69f0a
jadams41 <theride40@gmail.com>
2016-01-26 15:44:16 -0800

    Added MailChimp Support to the Subscribe panel

--- js/scripts.js
+++ js/scripts.js
@@ -10,30 +10,8 @@ $(document).ready(function () {/* smooth scrolling for scroll to top */
     /* highlight the top nav as scrolling occurs */
     $('body').scrollspy({target: '#navbar'})
 
-    $('#subscribe-button').click(function(){
-        var firstName = $('#first-name-input').val();
-        var lastName = $('#last-name-input').val();
-        var email = $('#email-input').val();
-    });
 });
 
-$( "#whitehomepage" ).click(function() {
-    formData = {
-        u: "3959eeadb32e02b85a792e21c",
-        id: "6d7613df26"
-    };
-    $.ajax({
-        url: "http://fiercelycurious.us1.list-manage.com/subscribe/post",
-        type: "POST",
-        crossDomain: true,
-        contentType: 'application/json',
-        data: formData,
-        dataType: "json",
-        success: function(data) {
-            //success handler
-        },
-        error: function() {
-            //error handler
-        }
-    });
+$('#mce-success-response').bind('DOMSubtreeModified', function(){
+    $('#mc-embedded-subscribe-form').slideUp();
 });
